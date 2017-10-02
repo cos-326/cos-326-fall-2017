16264fd5eb5c982b66798a77924b509eb27f31b3
Will Ockelmann-Wagner <will@whentheresawill.net>
2017-10-02 02:59:12 -0700

    prettier tests

--- a1/test.ml
+++ a1/test.ml
@@ -1,34 +1,48 @@
-
 open OUnit2;;
 
-let test1 test_name = assert_bool "empty name" ((String.length A1.name) > 1 )
-
-let test2 test_email = assert_bool "email is not longer than name" ((String.length A1.name) > 1 )
-
-let test3 test_few_divisors = assert_bool "bad divisors" (A1.few_divisors 17 3) 
-let test4 test_few_divisors = assert_bool "bad divisors" (not (A1.few_divisors 4 3))
-let test4 test_few_divisors = assert_bool "bad divisors" (A1.few_divisors 4 4)
-let test5 test_few_divisors = assert_bool "bad divisors" (not (A1.few_divisors 18 6))
-let test6 test_few_divisors = assert_bool "bad divisors" (A1.few_divisors 18 7)
-
-let test7 test_approx = assert_equal (A1.sin_pi 0) 3.
-let test8 test_approx = assert_equal (int_of_float ((A1.sin_pi 1) *. 1000.)) 3141
-
-let test9 test_approx = assert_equal (int_of_float ((A1.monte_pi 10000) *. 10.)) 31
-
 let suite =
-  "suite">:::
-  ["test1">:: test1;
-   "test2">:: test2;
-   "test3">:: test3;
-   "test4">:: test4;
-   "test5">:: test5;
-   "test6">:: test6;
-   "test7">:: test7;
-   "test8">:: test8;
-   "test9">:: test9;
+  "A1" >::: [
+    "name" >:: (fun _ -> 
+        assert_bool "empty name" ((String.length A1.name) > 1 )
+      );
+
+    "email" >:: (fun _ -> 
+        assert_bool "email is not longer than name" ((String.length A1.name) > 1 )
+      );
+
+    "few_divisors 1"  >:: (fun _ -> 
+        assert_bool "bad divisors" (A1.few_divisors 17 3)
+      );
+
+    "few_divisors 2"  >:: (fun _ -> 
+        assert_bool "bad divisors" (not (A1.few_divisors 4 3))
+      );
+
+    "few_divisors 3"  >:: (fun _ -> 
+        assert_bool "bad divisors" (A1.few_divisors 4 4)
+      );
+
+    "few_divisors 4"  >:: (fun _ -> 
+        assert_bool "bad divisors" (not (A1.few_divisors 18 6))
+      );
+
+    "few_divisors 5"  >:: (fun _ -> 
+        assert_bool "bad divisors" (A1.few_divisors 18 7)
+      );
+
+    "sin_pi 0 rounds"  >:: (fun _ -> 
+        assert_equal (A1.sin_pi 0) 3.
+      );
+
+    "sin_pi 1 round"  >:: (fun _ -> 
+        assert_equal (int_of_float ((A1.sin_pi 1) *. 1000.)) 3141
+      );
+
+    "monte_pi"  >:: (fun test_ctxt -> 
+        let _ = logf test_ctxt `Info "monte_pi: %F" (A1.monte_pi 10000) in
+        assert_equal (int_of_float ((A1.monte_pi 10000) *. 10.)) 31
+      );
   ]
-;;
 
 let () = 
   run_test_tt_main suite
