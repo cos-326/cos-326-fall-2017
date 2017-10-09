40ad418dc155fa8d8e36b9fba8f3e5c341d7f25f
Chhi'med Kunzang <chhimed@carbonfive.com>
2017-10-08 23:23:56 -0700

    Add tests for sin_pi.

--- a1/test.ml
+++ a1/test.ml
@@ -10,6 +10,12 @@ let test4 test_few_divisors = assert_bool "bad divisors" (A1.few_divisors 4 4)
 let test5 test_few_divisors = assert_bool "bad divisors" (not (A1.few_divisors 18 6))
 let test6 test_few_divisors = assert_bool "bad divisors" (A1.few_divisors 18 7)
 
+let test7 test_sin_pi = assert_equal 3.0 (A1.sin_pi 0)
+let test8 test_sin_pi = assert_equal true (cmp_float ~epsilon:0.0001 3.141 (A1.sin_pi 1))
+let test9 test_sin_pi = assert_equal true (cmp_float ~epsilon:0.0000000001 3.1415926535 (A1.sin_pi 2))
+let test10 test_sin_pi = assert_equal true (cmp_float ~epsilon:0.000000000000001 3.141592653589793 (A1.sin_pi 3))
+(* let test9 test_sin_pi = assert_equal 3.0 (A1.sin_pi 0) *)
+
 let suite =
   "suite">:::
   ["test1">:: test1;
@@ -18,6 +24,10 @@ let suite =
    "test4">:: test4;
    "test5">:: test5;
    "test6">:: test6;
+   "test7">:: test7;
+   "test8">:: test8;
+   "test9">:: test9;
+   "test10">:: test10;
   ]
 ;;
 
