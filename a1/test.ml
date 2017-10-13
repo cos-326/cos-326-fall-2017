9366134767296a9f0422fecdfeb616d5eb8257ea
Will Ockelmann-Wagner <will@whentheresawill.net>
2017-10-13 01:45:02 -0700

    unskip a1

--- a1/test.ml
+++ a1/test.ml
@@ -3,62 +3,62 @@ open OUnit2;;
 let suite =
   "A1" >::: [
     "name" >:: (fun _ -> 
-        skip_if true "skip";
+
         assert_bool "empty name" ((String.length A1.name) > 1 )
       );
 
     "email" >:: (fun _ -> 
-        skip_if true "skip";
+
         assert_bool "email is not longer than name" ((String.length A1.name) > 1 )
       );
 
     "few_divisors 1"  >:: (fun _ -> 
-        skip_if true "skip";
+
         assert_bool "bad divisors" (A1.few_divisors 17 3)
       );
 
     "few_divisors 2"  >:: (fun _ -> 
-        skip_if true "skip";
+
         assert_bool "bad divisors" (not (A1.few_divisors 4 3))
       );
 
     "few_divisors 3"  >:: (fun _ -> 
-        skip_if true "skip";
+
         assert_bool "bad divisors" (A1.few_divisors 4 4)
       );
 
     "few_divisors 4"  >:: (fun _ -> 
-        skip_if true "skip";
+
         assert_bool "bad divisors" (not (A1.few_divisors 18 6))
       );
 
     "few_divisors 5"  >:: (fun _ -> 
-        skip_if true "skip";
+
         assert_bool "bad divisors" (A1.few_divisors 18 7)
       );
 
     "sin_pi round 0"  >:: (fun _ -> 
-        skip_if true "skip";
+
         assert_equal (A1.sin_pi 0) 3.
       );
 
     "sin_pi round 1"  >:: (fun _ -> 
-        skip_if true "skip";
+
         assert_bool "wrong pi" (cmp_float ~epsilon:0.001 (A1.sin_pi 1) 3.141)
       );
 
     "sin_pi round 2"  >:: (fun _ -> 
-        skip_if true "skip";
+
         assert_bool "wrong pi" (cmp_float ~epsilon:0.0000000001 (A1.sin_pi 2) 3.1415926535)
       );
 
     "sin_pi round 3"  >:: (fun _ -> 
-        skip_if true "skip";
+
         assert_bool "wrong pi" (cmp_float ~epsilon:0.000000000000001 (A1.sin_pi 3) 3.141592653589793)
       );
 
     "monte_pi"  >:: (fun _ -> 
-        skip_if true "skip";
+
         Printf.printf "\nmonte_pi answer: %F\n" (A1.monte_pi 10000);
         assert_bool "wrong Pi" (cmp_float ~epsilon:0.1 (A1.monte_pi 10000) 3.1)
       );
