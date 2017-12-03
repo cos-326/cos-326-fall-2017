37da04ef68e1195634c8660a269a84e079f2a79d
Will Ockelmann-Wagner <will@whentheresawill.net>
2017-12-03 04:25:16 -0800

    fix functions

--- a4/EvalEnv.ml
+++ a4/EvalEnv.ml
@@ -23,6 +23,7 @@ let rec is_value (e:exp) : bool =
 
 (* evaluation; use eval_loop to recursively evaluate subexpressions *)
 let eval_body (env:env) (eval_loop:env -> exp -> exp) (e:exp) : exp = 
+  let eval_env = eval_loop env in
   match e with
   | Var x -> 
     (match lookup_env env x with 
@@ -30,47 +31,53 @@ let eval_body (env:env) (eval_loop:env -> exp -> exp) (e:exp) : exp =
      | Some v -> v)
   | Constant _ -> e
   | Op (x, op, y) ->
-    let v1 = eval_loop env x in 
-    let v2 = eval_loop env y in 
+    let v1 = eval_env x in 
+    let v2 = eval_env y in 
     apply_op v1 op v2 
   | If (condition, yes, no) ->
-    (match eval_loop env condition with
-     | Constant (Bool true) -> eval_loop env yes
-     | Constant (Bool false) -> eval_loop env no
+    (match eval_env condition with
+     | Constant (Bool true) -> eval_env yes
+     | Constant (Bool false) -> eval_env no
      | v1 -> raise (BadIf v1))
   | Let (var, x, expression) ->
-    eval_loop (update_env env var x) expression
-  | Pair(x, y) -> Pair(eval_loop env x, eval_loop env y)
+    let value = eval_env x in
+    eval_loop (update_env env var value) expression
+  | Pair(x, y) -> Pair(eval_env x, eval_env y)
   | Fst p -> 
-    (match eval_loop env p with
-     | Pair(x, _) -> eval_loop env x
+    (match eval_env p with
+     | Pair(x, _) -> eval_env x
      | p -> raise (BadPair p))
   | Snd p -> 
-    (match eval_loop env p with
-     | Pair(_, y) -> eval_loop env y
+    (match eval_env p with
+     | Pair(_, y) -> eval_env y
      | p -> raise (BadPair p))
   | EmptyList -> EmptyList
   | Cons (hd, tl)-> 
-    let hd = eval_loop env hd in
-    (match eval_loop env tl with
+    let hd = eval_env hd in
+    (match eval_env tl with
      | EmptyList -> Cons(hd, EmptyList)
      | Cons(x,y) -> Cons(hd, Cons(x,y))
      | _ -> raise (BadMatch tl))
   | Match (matcher, if_empty, hd, tl, if_full)-> 
     (match matcher with
-     | EmptyList -> eval_loop env if_empty
+     | EmptyList -> eval_env if_empty
      | Cons(h, t) ->
        let env = update_env env hd h in
        let env = update_env env tl t in
-       eval_loop env if_full
+       eval_env if_full
      | _ -> raise (BadMatch matcher))
   | Rec(name, param, body)->
     Closure(env, name, param, body)
   | Closure _-> e
   | App(f, arg)->
-    (match f with
-     | Closure(env, name, param, body) ->
-       eval_loop (update_env env param arg) body
+    (match eval_env f with
+     | Closure(c_env, name, param, body) ->
+       let arg = eval_env arg in
+
+       let c_env = update_env c_env name (Closure(c_env, name, param, body)) in
+       let c_env = update_env c_env param arg in
+
+       eval_loop (update_env c_env param arg) body
      | _ -> raise (BadApplication f))
 
 (* evaluate closed, top-level expression e *)
--- a4/Main.ml
+++ a4/Main.ml
@@ -4,4 +4,4 @@
 (* switch EvalEnv to EvalSubst to test substitution-based interpreter *)
 module Eval = EvalEnv
 
-let main = Testing.run_tests Eval.eval Testing.tests
+let main = Testing.run_tests Eval.debug_eval Testing.tests
