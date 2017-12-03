3cde6c160dca7122b66527294f90a6ad89dbec8b
Will Ockelmann-Wagner <will@whentheresawill.net>
2017-12-03 03:51:41 -0800

    first pass at EvalEnv

--- a4/EvalEnv.ml
+++ a4/EvalEnv.ml
@@ -26,21 +26,52 @@ let eval_body (env:env) (eval_loop:env -> exp -> exp) (e:exp) : exp =
   match e with
   | Var x -> 
     (match lookup_env env x with 
-       None -> raise (UnboundVariable x)
+     | None -> raise (UnboundVariable x)
      | Some v -> v)
-  | Constant c -> failwith "unimplemented"
-  | Op (x, op, y) -> failwith "unimplemented"
-  | If (condition, yes, no) -> failwith "unimplemented"
-  | Let (var, x, expression) -> failwith "unimplemented"
-  | Pair(x, y) -> failwith "unimplemented"
-  | Fst x -> failwith "unimplemented"
-  | Snd y -> failwith "unimplemented"
-  | EmptyList -> failwith "unimplemented"
-  | Cons (hd, tl)-> failwith "unimplemented"
-  | Match (matcher, if_empty, hd, tl, if_full)-> failwith "unimplemented"
-  | Rec(name, param, body)-> failwith "unimplemented"
-  | Closure(env, name, params, body)-> failwith "unimplemented"
-  | App(name, arg)-> failwith "unimplemented"
+  | Constant _ -> e
+  | Op (x, op, y) ->
+    let v1 = eval_loop env x in 
+    let v2 = eval_loop env y in 
+    apply_op v1 op v2 
+  | If (condition, yes, no) ->
+    (match eval_loop env condition with
+     | Constant (Bool true) -> eval_loop env yes
+     | Constant (Bool false) -> eval_loop env no
+     | v1 -> raise (BadIf v1))
+  | Let (var, x, expression) ->
+    eval_loop (update_env env var x) expression
+  | Pair(x, y) -> Pair(eval_loop env x, eval_loop env y)
+  | Fst p -> 
+    (match eval_loop env p with
+     | Pair(x, _) -> eval_loop env x
+     | p -> raise (BadPair p))
+  | Snd p -> 
+    (match eval_loop env p with
+     | Pair(_, y) -> eval_loop env y
+     | p -> raise (BadPair p))
+  | EmptyList -> EmptyList
+  | Cons (hd, tl)-> 
+    let hd = eval_loop env hd in
+    (match eval_loop env tl with
+     | EmptyList -> Cons(hd, EmptyList)
+     | Cons(x,y) -> Cons(hd, Cons(x,y))
+     | _ -> raise (BadMatch tl))
+  | Match (matcher, if_empty, hd, tl, if_full)-> 
+    (match matcher with
+     | EmptyList -> eval_loop env if_empty
+     | Cons(h, t) ->
+       let env = update_env env hd h in
+       let env = update_env env tl t in
+       eval_loop env if_full
+     | _ -> raise (BadMatch matcher))
+  | Rec(name, param, body)->
+    Closure(env, name, param, body)
+  | Closure _-> e
+  | App(f, arg)->
+    (match f with
+     | Closure(env, name, param, body) ->
+       eval_loop (update_env env param arg) body
+     | _ -> raise (BadApplication f))
 
 (* evaluate closed, top-level expression e *)
 
